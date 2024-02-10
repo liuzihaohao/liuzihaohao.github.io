cefe887e07f03402266c964c8980a8e190f35527
liuzihaohao <liuzihaohao@users.noreply.github.com>
2024-02-10 20:10:12 +0800

    Signed-off-by: liuzihaohao <liuzihaohao@users.noreply.github.com>

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -6,9 +6,69 @@ document.querySelectorAll('.zoom').forEach(item => {
 
 var taglist=[
     {
-        "TagName": "2023",
-        "background": "cornflowerblue",
-        "color": "white"
+        "TagName": "年度总结",
+        "background": "#3ab74f",
+        "color": "black"
+    },
+    {
+        "TagName": "信息学",
+        "background": "#673AB7",
+        "color": "black"
+    },
+    {
+        "TagName": "项目作品",
+        "background": "#3a66b7",
+        "color": "black"
+    },
+    {
+        "TagName": "Arduino",
+        "background": "#b73a3a",
+        "color": "black"
+    },
+    {
+        "TagName": "硬件开发",
+        "background": "#b7b53a",
+        "color": "black"
+    },
+    {
+        "TagName": "运维",
+        "background": "#b77d3a",
+        "color": "black"
+    },
+    {
+        "TagName": "化学",
+        "background": "#b73a9c",
+        "color": "black"
+    },
+    {
+        "TagName": "学习-Old",
+        "background": "aquamarine",
+        "color": "black"
+    },
+    {
+        "TagName": "小型编程练习",
+        "background": "chartreuse",
+        "color": "black"
+    },
+    {
+        "TagName": "Python",
+        "background": "darkorange",
+        "color": "black"
+    },
+    {
+        "TagName": "前端",
+        "background": "lightsalmon",
+        "color": "black"
+    },
+    {
+        "TagName": "Django",
+        "background": "sienna",
+        "color": "black"
+    },
+    {
+        "TagName": "回忆录-2023",
+        "background": "chartreuse",
+        "color": "black"
     }
 ]
 var all_tag=document.getElementsByClassName("md-tag");

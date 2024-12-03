59f0ce5248ee672a3dc67b8284394984e8e7f120
liuzihaohao <liuzihaohao@users.noreply.github.com>
2024-12-03 17:07:32 +0800

    deploy: 9fd3405c371f4a875513162193135b85a5f21f92

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -4,73 +4,21 @@ document.querySelectorAll('.zoom').forEach(item => {
     })
 });
 
-var taglist=[
-    {
-        "TagName": "年度总结",
-        "background": "#3ab74f",
-        "color": "black"
-    },
-    {
-        "TagName": "信息学",
-        "background": "#673AB7",
-        "color": "black"
-    },
-    {
-        "TagName": "项目作品",
-        "background": "#3a66b7",
-        "color": "black"
-    },
-    {
-        "TagName": "Arduino",
-        "background": "#b73a3a",
-        "color": "black"
-    },
-    {
-        "TagName": "硬件开发",
-        "background": "#b7b53a",
-        "color": "black"
-    },
-    {
-        "TagName": "运维",
-        "background": "#b77d3a",
-        "color": "black"
-    },
-    {
-        "TagName": "化学",
-        "background": "#b73a9c",
-        "color": "black"
-    },
-    {
-        "TagName": "学习-Old",
-        "background": "aquamarine",
-        "color": "black"
-    },
-    {
-        "TagName": "小型编程练习",
-        "background": "chartreuse",
-        "color": "black"
-    },
-    {
-        "TagName": "Python",
-        "background": "darkorange",
-        "color": "black"
-    },
-    {
-        "TagName": "前端",
-        "background": "lightsalmon",
-        "color": "black"
-    },
-    {
-        "TagName": "Django",
-        "background": "sienna",
-        "color": "black"
-    },
-    {
-        "TagName": "回忆录-2023",
-        "background": "chartreuse",
-        "color": "black"
-    }
-]
+var taglist=[]
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "/tag.json", false);
+try {
+  xhr.send();
+  if (xhr.status === 200) {
+    taglist = JSON.parse(xhr.responseText);
+  } else {
+    console.error(`ERROR: ${xhr.status}`);
+  }
+} catch (error) {
+  console.error("ERROR: ", error);
+}
+
+
 var all_tag=document.getElementsByClassName("md-tag");
 for(var i=0;i<all_tag.length;i++){
     var flag=true;
